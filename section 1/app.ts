3882b3f9e4417aca22f21cc0af06fbcc1aa724a1
markf <markfitz815@gmail.com>
2023-01-30 11:03:57 +0000

    Start using types

--- section 1/app.ts
+++ section 1/app.ts
@@ -1,1 +1,18 @@
-console.log('Your code goes here...');
+const add = (n1: number, n2: number, showResult: boolean) => {
+  const result = n1 + n2;
+
+  if (showResult) {
+    console.log(result);
+  } else {
+    return result;
+  }
+  return n1 + n2;
+};
+
+const number1 = 5;
+const number2 = 2.8;
+const printResult = true;
+
+const result = add(number1, number2, printResult);
+
+console.log(result);
