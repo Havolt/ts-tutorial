6908381e17550503d637fea9efcfb9e1d952d4db
markf <markfitz815@gmail.com>
2023-01-31 12:56:11 +0000

    different funtion types

--- functions.ts
+++ functions.ts
@@ -2,8 +2,23 @@ const adder = (n1: number, n2: number) => {
   return n1 + n2;
 };
 
+const addAndHandle = (n1: number, n2: number, cb: (num: number) => void ) {
+   cb(n1 + n2);
+}
+
 const printOutResult = (num: number): void => {
   console.log("Result: " + num);
 };
 
-printOutResult(adder(5, 12));
+// let combineValues: Function;
+let combineValues: (a: number, b:number) => number;
+
+combineValues = adder;
+
+
+console.log(combineValues(8, 8));
+
+
+// printOutResult(adder(5, 12, (result) => { console.log(result)}));
+
+addAndHandle(5, 12, (result) => { console.log(result)});
