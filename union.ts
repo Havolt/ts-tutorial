e11dbd9c0ab4852d52b1b7e785d52a1ad12ec8c9
markf <markfitz815@gmail.com>
2023-01-31 08:30:13 +0000

    add literal types example

--- union.ts
+++ union.ts
@@ -1,16 +1,20 @@
-const combine = (n1: number | string, n2: number | string) => {
+const combine = (n1: number | string, n2: number | string, resultConversion: 'as-number' | 'as-text') => {
   let result;
-  if (typeof n1 === "number" && typeof n2 === "number") {
-    result = n1 + n2;
+  if (typeof n1 === "number" && typeof n2 === "number" || resultConversion === 'as-number') {
+    result = +n1 + +n2;
   } else {
     result = n1.toString() + n2.toString();
   }
 
-  return result;
+  if(resultConversion === 'as-number' ) {
+    return +result;
+  } else {
+    result.toString();
+  }
 };
 
-const combineAges = combine(30, 26);
+const combineAges = combine(30, 26, 'as-number';
 console.log(combineAges);
 
-const combinedNames = combine("Mark", "Cait");
+const combinedNames = combine("Mark", "Cait", 'as-text');
 console.log(combinedNames);
